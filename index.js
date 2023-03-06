c0ad928b7ead5b67a713d0e7147d3a0b52d3cacd
dryelm <rinat.gilemshinn@gmail.com>
2023-03-06 17:46:16 +0500

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function convertStringToNumber(str){
-    let num = parseFloat(str)
-    if (isNaN(num)){
-        return false
-    }
-    return num
+function formatDate (date) {
+    const formatFunc = (x => x.toString().padStart(2, '0'));
+    const day = formatFunc(date.getDate());
+    const month = formatFunc(date.getMonth() + 1);
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
 }
-
-console.log(convertStringToNumber("fdsf"))
